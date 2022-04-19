262e234215027c912a9c717b0971a3118252b5e1
Yatharth Jain <Jainyat@sheridancollege.ca>
2022-04-18 20:18:03 -0400

    First Command with createSensor method without exception handling and greeting message

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 from wirelessNetwork import WirelessNetwork
 class Application():
-    
     def __init__(self):
         self._listOfSensors = []
         self._noOfNeighbours =0
@@ -8,154 +7,33 @@ class Application():
         self._neighId = ""
         self._dist =0
         self._path =[]
-        
     
     def createSensors(self,listOfSensors =[]):
         noOfSensors = input("Enter the number of sensors: ")
         self.setNoOfSensors(noOfSensors)
-        #The loop checks if noOfSensors is int or not
-        while(noOfSensors.isnumeric() == False):
-                print("This is an invalid entry for the number of sensors!")
-                noOfSensors =input("Re - enter the number of sensors: ")  
         for index in range(int(noOfSensors)):
-            
             id = input("Enter the sensors id: ")
             self.setId(id)
-            #This snippet checks if id is int
-            #sen = "Sensor " + id
             
-            
-            while(id.isnumeric() == True):
-                print("This is an invalid entry for sensor Id! ")
-                print("_*__*__*__*__*__*__*__*__*__*_ ")
-                id = input(print("Enter the Sensor Id: "))
-                self.setId(id)
-                
-            #This is when id =A!
-            while(('!' in id) == True):    
-                print("This is an invalid entry for sensor Id! ")
-                print("_*__*__*__*__*__*__*__*__*__*_ ")         
-                id = input(print("Enter the Sensor Id: "))
-            
-            #listOfSensors.append(id)
             listOfSensors += [[id]]
-            
             noOfNeighbours =input("Enter the number of neighbours: ")
             self.setNoOfNeighbours(noOfNeighbours)
-            while(noOfNeighbours.isnumeric() == False):
-                try:
-                    nNeigh = None
-                    nNeigh =int(noOfNeighbours)
-                    
-                except Exception:
-                    print("This is an invalid entry for the number of neighbours! ")  
-                    noOfNeighbours =input("Enter the number of Neighbours: ")  
-                    self.setNoOfNeighbours(noOfNeighbours)
             
             #This snippet creates number of neighbours with ids and distance from first Sensor id 
-            if(noOfNeighbours !=0):
-                
-                for index in range(int(noOfNeighbours)):
-                    neighId = input(print("Enter the neighbour for Sensor ",id,": "))
-                    self.setNeighId(neighId)
-                    while(neighId.isnumeric() ==True):
-                        print("This is an invalid entry for the neighbour’s name and/or distance! ")
-                        neighId = input(print("Enter the neighbour Id for Sensor ",id,": "))
-                        self.setNeighId(neighId)
-                    while(('!' in neighId) == True):    
-                        print("This is an invalid entry for the neighbour’s name and/or distance! ")
-                        neighId = input(print("Enter the neighbour for Sensor ",id,": "))      
-                        self.setNeighId(neighId)
-                    
-                    #listOfSensors.append(neighId)
-                    dist =input(print("Enter the distance to ",neighId,": "))
-                    while(dist.isnumeric() ==False):
-                        print("This is an invalid entry for the oxygen level! ")
-                        dist = input("Enter the Oxygen level in %: ")
-                        
-                    listOfSensors += [[neighId,int(dist)]]
-                
+            for index in range(int(noOfNeighbours)):
+                neighId = input(print("Enter the neighbour for Sensor ",id,": "))
+                self.setNeighId(neighId)
+                dist =input(print("Enter the distance to ",neighId,": "))
+                listOfSensors += [[neighId,int(dist)]]
             oxlevel = input("Enter the Oxygen level in %: ")
-            
-            while(oxlevel.isnumeric() ==False):
-                print("This is an invalid entry for the oxygen level! ")
-                oxlevel = input("Enter the Oxygen level in %: ")
-                self.setOxLevel(oxlevel)
-            
-            
             temp =input("Enter the temperature measurement: ")
             self.setTemprature(temp)
-            res = False
-            while(res == False):
-                try:
-                    float(temp)
-                    res =True
-                except :
-                    print("This is an invalid entry for the temperature! ")
-                    temp =input("Enter the temperature measurement: ")
-                    self.setTemprature(temp)
-                    res =False
             print("_*__*__*__*__*__*__*__*__*__*_ ")        
             listOfSensors += [[oxlevel,temp]]
             self.setTemprature(temp)
             self.setListOfSensors(listOfSensors)
         print(listOfSensors)
             
-    def convrtToDictionary(self):
-        self._dict = {}
-        listOfSensors = self._listOfSensors
-        self.setListOfSensors(listOfSensors)
-        '''
-        if(self._neighId !=""):
-            str ='distance with neighbour: '+self._neighId+": "
-            disindex = listOfSensors.index(str)
-            dict={"sensor id":self._id,"neighbour id":self._neighId,"distance from neighbour "+self._neighId:listOfSensors[disindex+1]}
-        else:
-            dict={"sensor id":self._id,"neighbour id":"No neighbour"}
-        '''
-        for index in range(len(listOfSensors)):
-            dict[listOfSensors[index][0][0]] = listOfSensors[index][1]
-            
-        self.setDict(dict)
-        
-    def findPath(self):
-        l =[]
-        sor = input("Enter the source sensor: ")
-        while True:
-            if(sor.isupper) and (sor in self.dict):
-                self._path.append(sor)
-                break
-            else:
-                print("Invalid Sensor Entered!")
-                sor = input("Renter the source sensor:")
-                continue
-        des = input(print("Enter the destination sensor: "))
-        while True:
-            if (des.isupper) and (des in self._dict):
-                if des in self._path:
-                    print("Path = ",self._path)
-                break
-            else:
-                print("Invalid Sensor Entered!")
-                des = input("Renter the destination sensor:")
-                continue
-        
-        for index in self._dict[sor]:
-            l.append(index[1])
-            m = max(l)
-        for index in self._dict[sor]:    
-            if index[0] == str(des):
-                self._path.append(index[0])
-                break
-            elif (index[1] == m) and (index[0] not in self._path):
-                self._path.append(index[0])
-                self.findPath(str(index[0]),des,self._path)
-            elif (index[1] == m) and (index[0] in self._path):
-                l.remove(index[1])
-                if l==[]:
-                    print("The destination is not found")
-                else:
-                    m = max(l)
     def setNoOfSensors(self,noOfSensors):
         self._noSensors = noOfSensors
 
@@ -197,16 +75,6 @@ class Application():
 
     def getListOfSensors(self):
         return self._listOfSensors    
-
-    def setDict(self,dict):
-        self._dict =dict
-
-    def getDict(self):
-        return self._dict
    
-WirelessNetwork.greetingMessage()
-WirelessNetwork.brand()
 ob =Application()
 ob.createSensors() 
-ob.convrtToDictionary()
-ob.findPath()
